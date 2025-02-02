1e81078159d2fe32d35a7c0dd6b0b138518a191b
Divyanshu Dhruv <divyanshudhruv24@gmail.com>
2025-02-02 12:32:52 +0530

    ♻️ refactor(code): Refactor the code and improve maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import React from "react";
 import App from "./App";
 import "remixicon/fonts/remixicon.css";
-// import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
